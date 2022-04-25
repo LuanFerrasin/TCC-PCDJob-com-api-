0213789f3fae0f2128816dbee14761e4dfa7f10d
LuanFerrasin <89094679+LuanFerrasin@users.noreply.github.com>
2022-04-25 16:40:53 -0300

    Candidato Cadastrando

---
+++ js/empresa.js
@@ -0,0 +1,91 @@
+'use strict';
+
+const getList = (string) => {
+    const variable = document.getElementById(string).value
+    const array = [variable]
+    return array
+}
+
+const validacao = ({nome, email, senha, telefone}) => {
+
+    if(nome.length > 3 && nome.length <= 50 && email.length > 9 && email.length <= 100  && senha.length > 3 && senha.length <= 50 && telefone.length > 1 && telefone.length <= 15) {
+        return true
+
+    } else {
+        return false
+    }
+
+} 
+
+const cadastrarEmpresa = async() => {
+    const empresa = {
+        nome: document.getElementById('nome').value,
+        email: getList('email'),
+        senha: document.getElementById('senha').value,
+        telefone: getList('telefone'),
+        areaAtuacao: document.getElementById('area-atuacao').value
+        
+    }
+
+    if(validacao(empresa)) {
+         await postEmpresa(empresa)
+    } else {
+        alert("preencha os campos vazios")
+    }
+}
+
+document.getElementById('btnCadastro').addEventListener('click', cadastrarEmpresa)
+
+
+const postEmpresa =  async(empresa) => {
+    const urlCadastro = 'http://10.107.144.26:8080/empresa/cadastrar'
+    const options = {
+        method: 'POST',
+        body:JSON.stringify(empresa),
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept':'application/json',
+        }
+    }
+
+    await fetch(urlCadastro, options).then(resp=>console.log(resp))
+   
+
+}
+
+const putProduto = async (empresa) => {
+    const urlAtualizar = 'http://10.107.144.26:8080/empresa/atualizar/'
+    const options = {
+        method: 'PUT',
+        body: JSON.stringify(empresa),
+        headers: {
+            'content-Type' : 'application/json'
+        }
+    }
+
+    await fetch(`${urlAtualizar}${empresa.id}`, options)
+}
+
+const deleteProduto = async (empresa) => {
+    const urldeletar = 'http://10.107.144.26:8080/empresa/deletar/'
+    const options = {
+        method: 'DELETE',
+        headers: {
+            'content-Type' : 'application/json'
+        }
+    }
+
+    await fetch(`${urldeletar}${empresa.id}`, options)
+}
+
+const getEmpresa = () => {
+
+    const urlListar = 'http://10.107.144.26:8080/empresa/listar'
+    const options = {
+        method: 'GET',
+    }
+    fetch(urlListar, options).then(resp=>console.log(resp))
+
+}
+
+
