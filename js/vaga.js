fbe0a15f1401b7810de04b4bd961b3363ddfe165
Daniel Gonçalves Batista <88516108+danielgbatista@users.noreply.github.com>
2022-05-04 16:57:33 -0300

    organização de arquivos

--- js/vaga.js
+++ js/vaga.js
@@ -1,51 +1,57 @@
 'use strict';
 
 
+    const getVaga = async() => {
 
-const postVaga =  async(vaga) => {
-    const urlCadastro = 'http://10.107.144.26:8080/vaga/cadastrar'
-    const options = {
-        method: 'POST',
-        body:JSON.stringify(vaga),
-        headers: {
-            'Content-Type': 'application/json',
-            'Accept':'application/json',
-        }
+    const urlListar = 'http://10.107.144.26:8080/vaga/listar'
+    const response = await fetch(urlListar).then(resp => resp.json()).then(dados => dados.content).then(content => content.filter (item => item.status == 0))
+    return response
+
     }
 
-    await fetch(urlCadastro, options).then(resp=>console.log(resp.json(Object.id)))
-   
+    const criarVaga = ({titulo, empresa, localTrabalho, id, deficiencia}) => {
+        const vaga = document.createElement('div')
+        vaga.classList.add("vagas")
+        vaga.innerHTML = `
+                <div class="headerVagas">
+                    <div class="informacaoVaga">
+                        <div class="imgEmpresa">
+                            <img src="../img/logoEmpresa.png" alt="">
+                        </div>
+                        <div class="conteudoVaga">
+                            <h1>${titulo}</h1>
+                            <div class="empresaVaga">
+                                <img src="../img/empresa.png" alt=""> 
+                                <p>${empresa.empresa}</p> 
+                            </div>
+                            <div class="empresaVaga">
+                                <img src="../img/local.png" alt=""> 
+                                <p>${localTrabalho.cidade} - ${localTrabalho.sigla}</p> 
+                            </div>
+                        </div>
+                    </div>
+                    
+                </div>
+                <div class="footerVagas">
+                    <div class="tipoDeficiencia">
+                        <h2>Deficiencia: ${deficiencia.map(item => item.tipoDeficiencia)}</h2>
+                    </div>
+                    <div class="botaoSaibaMais">
+                        <button id="btnSaibaMais" onClick="abrirModal(${id})">Saiba mais</button>
+                    </div>
+                </div>
+        `
 
-}
-
-const putVaga = async (vaga) => {
-    const urlAtualizar = 'http://10.107.144.26:8080/vaga/atualizar/'
-    const options = {
-        method: 'PUT',
-        body: JSON.stringify(vaga),
-        headers: {
-            'content-Type' : 'application/json'
-        }
+        return vaga;
     }
 
-    await fetch(`${urlAtualizar}${vaga.id}`, options)
-}
-
-const deleteProduto = async (vaga) => {
-    const urldeletar = 'http://10.107.144.26:8080/vaga/deletar/'
-    const options = {
-        method: 'DELETE',
-        headers: {
-            'content-Type' : 'application/json'
-        }
+    const carregarVaga = async() => {
+    const container = document.querySelector('.vagasConteiner')
+    const vagas = await getVaga();
+    const colunas = vagas.map(criarVaga)
+    container.replaceChildren(...colunas)
     }
-
-    await fetch(`${urldeletar}${vaga.id}`, options)
-}
-
-
 
 // document.querySelector("#btnSaibaMais").addEventListener("click", abrirModal);
-
 
 carregarVaga();
