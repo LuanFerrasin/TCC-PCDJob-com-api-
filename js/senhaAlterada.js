5319d2ce9698647e2091ae2d598b63e180cd4633
LuanFerrasin <89094679+LuanFerrasin@users.noreply.github.com>
2022-05-16 16:07:47 -0300

    testando libras no mobille

---
+++ js/senhaAlterada.js
@@ -0,0 +1,8 @@
+"use strict";
+
+function voltarLogin(){
+
+    window.location.href="../candidato/login.html";
+}
+
+document.getElementById('botao-formulario').addEventListener('click' , voltarLogin)
